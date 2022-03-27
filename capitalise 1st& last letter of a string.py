1ed730781caf1809df6a2884e919b37a163678ed
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2022-03-27 19:06:18 +0530

    Add files via upload

---
+++ capitalise 1st& last letter of a string.py
@@ -0,0 +1,6 @@
+#to capitasize 1st & last letter of a string.
+n=input()
+b=n[0].upper()
+c=n[-1].upper()
+d=n[1:-2: ]
+print(b+d+c)
---
+++ swap string character.py
@@ -0,0 +1,5 @@
+#enter two string through user first 2 element of first string swap with the first two element of second string.
+a=input()
+b=input()
+c=a[0:2]+b[2: ]
+print(c)
